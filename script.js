967a651dab7c47aaf2c1c138d0adb92682b540b4
Anthony Almeida <80277056+anthonyzzin@users.noreply.github.com>
2022-01-23 16:22:08 -0300

    3° Desafio em JS de 2022

---
+++ script.js
@@ -0,0 +1,20 @@
+for(i =1 ; i<=10;i++ ){
+    console.log(i)
+}
+
+let contaDecrescente = 10
+while(contaDecrescente >=1){
+    console.log(contaDecrescente --)
+}
+
+for(i=1; i <= 100; i++ ){
+    if(i % 2 !=0){
+        console.log(i)
+    }
+}
+
+for(i=1; i <= 100; i++ ){
+    if(i % 2 ==0){
+       console.log(i)
+   }
+}
